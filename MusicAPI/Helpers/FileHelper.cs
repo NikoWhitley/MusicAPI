03c04c3170aecdbf55720f4b1b67aa1d074590c2
Niko Whitley <william.n.whitley@gmail.com>
2022-07-13 11:21:54 -0500

    Add project files.

---
+++ MusicAPI/Helpers/FileHelper.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using MusicAPI.Models;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MusicAPI.Helpers
+{
+    public static class FileHelper
+    {
+        public static async Task<string> UploadImage(IFormFile file) 
+        {
+            string connectionString = @"DefaultEndpointsProtocol=https;AccountName=nikomusicstorageaccount;AccountKey=W4ue7JVmiVn/3oMx3Q2TPEK5u3J8FlIIQwTUd4cnEdOdWcJJZV2UCyrDMxHc5jdHE6ewY4nWUxew+AStgK+fcA==;EndpointSuffix=core.windows.net";
+            string containerName = "songscover";
+
+            BlobContainerClient blobContainerClient = new BlobContainerClient(connectionString, containerName);
+            BlobClient blobClient = blobContainerClient.GetBlobClient(file.FileName);
+            var memoryStream = new MemoryStream();
+            await file.CopyToAsync(memoryStream);
+            memoryStream.Position = 0;
+            await blobClient.UploadAsync(memoryStream);
+            return blobClient.Uri.AbsoluteUri;
+        }
+    }
+}
---
+++ MusicAPI/Models/Song.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicAPI.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Duration { get; set; }
+        public DateTime UploadDate { get; set; }
+        public bool IsFeatured { get; set; }
+
+        [NotMapped]
+        public IFormFile Image { get; set; }
+        public string ImageUrl { get; set; }
+
+        [NotMapped]
+        public IFormFile AudioFile { get; set; }
+        public string AudioUrl { get; set; }
+        public int ArtistId { get; set; }
+        public int? AlbumId { get; set; }
+    }
+}
